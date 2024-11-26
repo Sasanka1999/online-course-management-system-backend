04d77084385be056418634c199abbe48cbcc19e8
Ssanka1999 <sasankapabasara3@gmail.com>
2024-11-26 16:29:53 +0530

    first commit

---
+++ src/test/java/com/sarasavi/onlineCourseMS/OnlineCourseMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sarasavi.onlineCourseMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineCourseMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
